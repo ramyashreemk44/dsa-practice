ef583f24bd41e2d10e98e64452062cbaa379cfad
ramyashreemk44 <ramyashreemk16@gmail.com>
2025-04-12 13:13:12 -0600

    Add solutions for Pivot Index

--- Java/src/ArraysDsa/PivotIndex.java
+++ Java/src/ArraysDsa/PivotIndex.java
@@ -1,4 +1,27 @@
 package ArraysDsa;
 
 public class PivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int num : nums) rightSum += num;
+
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if (rightSum == leftSum) return i;
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 7, 3, 6, 5, 6};
+        PivotIndex solver = new PivotIndex();
+        int result = solver.pivotIndex(nums);
+        System.out.println("Pivot Index: " + result);  // Output: 3
+    }
 }
+
